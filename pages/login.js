1033eb478f1d3396c4ef3f3f660045ae0dc27d88
Jayson Wong <jaysonwongf@gmail.com>
2023-05-28 20:23:34 +0800

    deploy

---
+++ pages/login.js
@@ -0,0 +1,16 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+export default function Login() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  if (session) {
+    router.push('/');
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+}
